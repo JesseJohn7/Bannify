aa4216b573d4f697e829e32e75f2c6d204b411ad
JesseJohn7 <Jessejohn260@gmail.com>
2026-04-02 15:53:20 +0100

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-export default function Home() {
-  return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import TrustedCompanies from "@/components/TrustedCompanies";
+import FeaturesSection from "@/components/FeaturesSection";
+import Testimonials from "@/components/Testimonials";
+import Pricing from "@/components/Pricing";
+import FaqSection from "@/components/FaqSection";
+import BottomBanner from "@/components/BottomBanner";
+
+export default function Page() {
+    return (
+        <>
+            <Navbar />
+            <HeroSection />
+            <TrustedCompanies />
+            <FeaturesSection />
+            <Testimonials />
+            <Pricing />
+            <FaqSection />
+            <BottomBanner />
+            <Footer />
+        </>
+    );
 }
