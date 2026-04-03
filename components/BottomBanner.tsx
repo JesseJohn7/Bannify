b374870cd6901c5bf4ad320e24a8cab6bde3dbf1
JesseJohn7 <Jessejohn260@gmail.com>
2026-04-03 11:35:45 +0100

    bottom

--- components/BottomBanner.tsx
+++ components/BottomBanner.tsx
@@ -1,4 +1,4 @@
-import { GithubIcon } from "lucide-react";
+import { Github } from "lucide-react";
 
 export default function BottomBanner() {
     return (
@@ -6,7 +6,7 @@ export default function BottomBanner() {
             <div className="flex flex-col md:flex-row text-center md:text-left items-center justify-between gap-8 px-3 md:px-10 border-x border-dashed border-slate-200 py-20 -mt-10 -mb-10 w-full">
                 <p className="text-xl font-medium max-w-sm">Join hundreds of developers building better SaaS products.</p>
                 <button className="flex items-center gap-2 rounded-md py-3 px-5 bg-indigo-600 hover:bg-indigo-700 transition text-white">
-                    <GithubIcon size={20} />
+                    <Github size={20} />
                     <span>Star on Github</span>
                 </button>
             </div>
